52bdecdf7a8b0b746712c714e6647aa943da1699
aditya <aadi.khare88@gmail.com>
2026-01-16 11:03:20 +0530

    change2

--- Test/src/test/java/DemoTest.java
+++ Test/src/test/java/DemoTest.java
@@ -3,11 +3,17 @@ import org.testng.annotations.Test;
 public class DemoTest {
 	
 	
-	@Test
+	@Test(groups= {"smokeTest"})
 	
 	public void demoTest() {
 		System.out.println("========test1=========ADITYA==========");
 		System.out.println("hiii");
 	}
 
+   @Test(groups= {"RT"})
+	
+	public void demoTest1() {
+		System.out.println("========test2===========khare========");
+		System.out.println("hiii");
+	}
 }
